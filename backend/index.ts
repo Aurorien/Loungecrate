24fa77b09a9aafbd5acc1954fb5fdc97c33a9904
Aurorien <starlightnoise@gmail.com>
2023-12-12 23:52:02 +0100

    Initial commit

---
+++ backend/index.ts
@@ -0,0 +1,16 @@
+import cors from "cors";
+import * as dotenv from "dotenv";
+import { Client } from "pg";
+import express from "express";
+
+dotenv.config();
+
+const client = new Client({
+  connectionString: process.env.PGURI,
+});
+
+client.connect();
+
+const app = express();
+
+app.use(cors());
