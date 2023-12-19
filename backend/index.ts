35dc550df89488570aee5403dc6481e52a9f0a78
Aurorien <starlightnoise@gmail.com>
2023-12-19 23:58:29 +0100

    Add endpoint, WIP

--- backend/index.ts
+++ backend/index.ts
@@ -11,26 +11,90 @@ const client = new Client({
 
 client.connect();
 
-const app = express();
+const app = express(),
+  port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(express.json(), cors());
 
-// Endpoint for adding user
+async function databaseConnection() {
+  try {
+    await client.connect();
+    console.log("Database is running and the connection is established.");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+}
+databaseConnection();
 
-// Endpoint for log in user
+// Endpoint for adding user, WIP
+app.post("/users", async (_request, response) => {
+  try {
+    const { userName, userPassword } = _request.body;
+
+    // Database transaction
+    await client.query("BEGIN");
+
+    if (userName) {
+      const addUserQuery = `INSERT INTO users (userName, userPassword) VALUES ($1, $2)`;
+      const userValues = [userName, userPassword];
+
+      await client.query(addUserQuery, userValues);
+    }
+
+    await client.query("COMMIT");
+
+    response.send("User successfully added into the database");
+  } catch (error) {
+    await client.query("ROLLBACK");
+
+    console.error("Error executing the SQL query:", error);
+    response.status(500).send("Internal Server Error");
+  }
+});
+
+// Endpoint for log in user, WIP
+app.post("/login", async (_request, response) => {
+  const { userName, userPassword } = _request.body;
+
+  try {
+    const result = await client.query(
+      "SELECT * FROM users WHERE username = $1 and userPassword = $2",
+      [userName, userPassword]
+    );
+
+    const user = result.rows[0];
+
+    if (!user) {
+      response.status(401).json({
+        success: false,
+        message: "No account found with that username or password",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
 
 // Endpoint for getting events for logged in user
+
+// Endpoint for adding city
+
+// Endpoint for adding venues
+
+// Endpoint for adding bands
 
 // Endpoint for adding event
 
 // Endpoint for searching or getting bands
 
-// Endpoint for adding bands
-
 // Endpoint for searching or getting venues
-
-// Endpoint for adding venues
 
 // Endpoint for searching or getting city
 
-// Endpoint for adding city
+app.listen(port, () => {
+  console.log(`Ready at http://localhost:${port}/`);
+});
