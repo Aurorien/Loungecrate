58b1c0f17e4de144159bd4b1616c917423a5b7d3
Aurorien <starlightnoise@gmail.com>
2023-12-21 16:02:06 +0100

    Bug fix

--- backend/init.sql
+++ backend/init.sql
@@ -37,7 +37,9 @@ CREATE TABLE events (
   eventDate DATE,
   eventTime VARCHAR(20),
   eventVenueId INT,
-  FOREIGN KEY (eventVenueId) REFERENCES venue(venueId)
+  eventUserId INT,
+  FOREIGN KEY (eventVenueId) REFERENCES venue(venueId),
+  FOREIGN KEY (eventUserId) REFERENCES users(userId)
 );
 
 CREATE TABLE eventBand (
@@ -53,7 +55,7 @@ CREATE TABLE rider (
   riderItemName VARCHAR(200),
   riderItemAmount VARCHAR(200),
   riderEventBandId INT,
-  FOREIGN KEY (riderBandId) REFERENCES EventBand(EventBandId)
+  FOREIGN KEY (riderEventBandId) REFERENCES eventBand(eventBandId)
 );
 
 ---------------------------------------------------------------------
@@ -62,45 +64,39 @@ INSERT INTO users (userName, userPassword)
 VALUES ('test', 'test');
 
 INSERT INTO city (cityName)
-VALUES (
-  'Göteborg',
-  'Tokyo',
-  'Lövstabruk',
-  'Berlin'
-  );
+VALUES
+  ('Göteborg'),
+  ('Tokyo'),
+  ('Lövstabruk'),
+  ('Berlin');
 
 INSERT INTO venue (venueName, venueSize, venueCityId)
-VALUES (
+VALUES
   ('Flowerave', 'Large', 1),
   ('The Dome', 'Huge', 2),
   ('Folkets Hus', 'Large', 3),
-  ('Waldrock', 'Medium', 4)
-);
+  ('Waldrock', 'Medium', 4);
 
 INSERT INTO band (bandName, bandGenre, bandDescription, bandCityId)
-VALUES (
+VALUES
   ('Myceline', 'darkwave', 'Out of Hisingsdimman emerges this darkwave band inspired by mushrooms and a connection with nature', 1),
   ('Yggdrasil', 'kraut', 'Analog, pure, and reverberating. Unique musical consistencies influenced by the acoustics of the radomes at Teufelsberg in Berlin.', 4),
-  ('Idril', 'ambient', 'Atmospheric, infinite depth and width. Idril takes us on a bottomless journey through fantastic soundscapes.', 3)
-);
+  ('Idril', 'ambient', 'Atmospheric, infinite depth and width. Idril takes us on a bottomless journey through fantastic soundscapes.', 3);
 
 INSERT INTO events (eventName, eventDescription, eventDate, eventTime, eventVenueId, eventUserId)
-VALUES (
+VALUES
   ('The Cave', 'Welcome into the dark and wonderous space where bats sings in choir.', '2025-04-28', '19:00', 2, 1),
-  ('Klubb Twilight', 'En på en resa i ljudlandskap med Myceline och Idril!', '2025-09-03', '19:00', 3, 1)
-)
+  ('Klubb Twilight', 'En på en resa i ljudlandskap med Myceline och Idril!', '2025-09-03', '19:00', 3, 1);
 
 INSERT INTO eventBand (eventBandEventId, eventBandBandId)
-VALUES (
+VALUES
   (1, 2), -- 1: Yggrasil på The Cave
   (2, 1), -- 2: Myceline på Klubb Twilight
-  (2, 3) -- 3: Idril på Klubb Twilight
-)
+  (2, 3); -- 3: Idril på Klubb Twilight
 
 
 INSERT INTO rider (riderItemName, riderItemAmount, riderEventBandId)
-VALUES (
+VALUES
   ('cola-soda', '1L', 1), ('fullgrain chocolate cookies', '10 pcs', 1),
   ('skumsvampar', '1/2 kg', 2), ('spenatsmoothie', '4 st.', 2), ('patchouli-rökelse', '1 st.', 2),
-  ('melonsoda', 'ett stort glas', 3), ('luftfuktare', '1 st.', 3)
-);
+  ('melonsoda', 'ett stort glas', 3), ('luftfuktare', '1 st.', 3);
