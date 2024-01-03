76e6e8d351a5801489cff19c22872fdc632cf4d0
Aurorien <starlightnoise@gmail.com>
2024-01-04 00:31:54 +0100

    Add e2e test of log in

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Loungecrate', () => {
+  it('is logging in', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('#username').type('Shadowood')
+    cy.get('#password').type('testuser')
+    cy.intercept('POST', '/login').as('postData')
+    cy.get('#submit').click()
+    cy.wait('@postData').then((interception) => {
+      const responseBody = interception.response.body
+      expect(responseBody).to.include({
+        success: true,
+        username: 'Shadowood'
+      })
+    })
   })
 })
