dfa7d8954a6ff49220321c57f7e938e425696485
Aurorien <starlightnoise@gmail.com>
2024-01-05 14:26:28 +0100

    ReAdd e2e test

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,5 +1,16 @@
 describe('Loungecrate', () => {
   it('is logging in', () => {
     cy.visit('http://localhost:5173/')
+    cy.get('#username').type('Shadowood')
+    cy.get('#password').type('testuser')
+    console.log('Submitting login form')
+    cy.intercept('POST', '/login').as('postData')
+    cy.get('#submit').click()
+    cy.wait('@postData').then((interception) => {
+      console.log('Login request intercepted', interception)
+      const responseBody = interception.response.body
+      console.log('Response Body:', responseBody.success)
+      expect(responseBody.success).to.equal(true)
+    })
   })
 })
