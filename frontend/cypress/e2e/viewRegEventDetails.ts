91b68f7cb4c052522ba5225d4b3df85965085082
Aurorien <starlightnoise@gmail.com>
2024-01-06 18:59:28 +0100

    Add BDD TDD test for feature (number 2)

---
+++ frontend/cypress/e2e/viewRegEventDetails.ts
@@ -0,0 +1,43 @@
+import { When, Then, Given } from '@badeball/cypress-cucumber-preprocessor'
+
+Given('Jag är inloggad, är på hemsidan och listan finns', () => {
+  cy.visit('http://localhost:5173/')
+  cy.intercept('POST', '/login', {
+    statusCode: 200,
+    body: {
+      success: true,
+      username: 'Shadowood'
+    }
+  }).as('loginRequest')
+  cy.get('#username').type('Shadowood')
+  cy.get('#password').type('testuser')
+  cy.get('#submit').click()
+  cy.wait('@loginRequest').then((interception) => {
+    expect(interception.response.statusCode).to.eq(200)
+    expect(interception.response.body.success).to.be.true
+    expect(interception.response.body.username).to.eq('Shadowood')
+  })
+  cy.get('[data-testid="event-list-h"]').should('have.text', 'My Events')
+  cy.get('[data-testid="event-list"]').should('be.visible')
+})
+
+When('Jag klickar på ett event', () => {
+  cy.get('[data-testid="event-item"]').first().click()
+})
+
+Then(
+  'Ska en modal dyka upp med samma rubrik som eventet och innehålla detaljerad info om eventet',
+  () => {
+    cy.get('[data-testid="event-modal"]').should('be.visible')
+    cy.get('[data-testid="event-item"]')
+      .first()
+      .invoke('text')
+      .then((eventTitle) => {
+        cy.get('[data-testid="event-modal-title"]').should(
+          'have.text',
+          eventTitle
+        )
+      })
+    cy.get('[data-testid="event-modal-detail"]').should('exist')
+  }
+)
