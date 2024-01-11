5bb60ed824d108ab5af364e634b95269df5d6c54
Aurorien <starlightnoise@gmail.com>
2024-01-12 00:42:06 +0100

    Add e2e "Add event" form test

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -22,3 +22,51 @@ describe('Loungecrate', () => {
     })
   })
 })
+
+describe('AddEvent form tests', () => {
+  it('should enable the submit button when the form is filled correctly', () => {
+    cy.visit('http://localhost:5173/')
+    cy.intercept('POST', '/login', {
+      statusCode: 200,
+      body: {
+        success: true,
+        username: 'Shadowood'
+      }
+    }).as('loginRequest')
+    cy.get('[data-testid="username"]').type('Shadowood')
+    cy.get('[data-testid="password"]').type('testuser')
+    cy.get('[data-testid="submit"]').click()
+    cy.wait('@loginRequest').then((interception) => {
+      expect(interception.response.statusCode).to.eq(200)
+      expect(interception.response.body.success).to.be.true
+      expect(interception.response.body.username).to.eq('Shadowood')
+    })
+    cy.get('[data-testid="home-add-event-button"]').should(
+      'have.text',
+      'Add event'
+    )
+    cy.get('[data-testid="home-add-event-button"]').click()
+    cy.url().should('include', '/addevent')
+
+    cy.get('[data-testid="addevent-submit-button"]').should('be.disabled')
+
+    cy.get('[data-testid="event-name"]').type('Sample Event')
+    cy.get('[data-testid="event-description"]').type(
+      'This is a sample event description.'
+    )
+    cy.get('[data-testid="date"]').type('2025-01-01')
+    cy.get('[data-testid="time"]').type('12:00')
+
+    cy.get('[data-testid="city-dropdown-input"]').click()
+    cy.get('[data-testid="city-dropdown-list"] li').first().click()
+
+    cy.get('[data-testid="venue-dropdown-input"]').click({ multiple: true })
+    cy.get('[data-testid="venue-dropdown-list"] li').first().click()
+
+    cy.get('[data-testid="bandlist"] [data-testid="bandlist-add"]')
+      .first()
+      .click()
+
+    cy.get('[data-testid="addevent-submit-button"]').should('be.enabled')
+  })
+})
