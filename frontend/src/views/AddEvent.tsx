740c806fa8f568a971a2ddc612162eb99199fc59
Aurorien <starlightnoise@gmail.com>
2024-01-13 22:33:15 +0100

    Add confirmation message and redirect in AddEvent

--- frontend/src/views/AddEvent.tsx
+++ frontend/src/views/AddEvent.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import DropdownFiltered from '../components/DropdownFiltered'
 import { useLogInStore } from '../utils/store'
 import BandList from '../components/BandList'
@@ -7,6 +8,7 @@ import { DropdownOption, Band } from '../utils/interfaces'
 import { errorHandling } from '../utils/errorHandling'
 
 function AddEvent() {
+  const navigate = useNavigate()
   const { username } = useLogInStore()
   const [cities, setCities] = useState<DropdownOption[]>([])
   const [allVenues, setAllVenues] = useState<DropdownOption[]>([])
@@ -19,6 +21,7 @@ function AddEvent() {
   const [selectedCity, setSelectedCity] = useState<DropdownOption | null>()
   const [selectedVenue, setSelectedVenue] = useState<DropdownOption | null>()
   const [selectedBands, setSelectedBands] = useState<Band[]>([])
+  const [confirmationVisible, setConfirmationVisible] = useState(false)
 
   useEffect(() => {
     fetch('/dropdowns')
@@ -34,8 +37,6 @@ function AddEvent() {
         errorHandling('POST', 'Error fetching dropdown data:', error)
       })
   }, [])
-
-  // console.log('bands', bands)
 
   const isFormValid = () => {
     return (
@@ -57,13 +58,10 @@ function AddEvent() {
     } else {
       setSelectedCity(city)
       if (city) {
-        // console.log('allVenues', allVenues)
-
         const filteredVenues = allVenues.filter(
           (venue) => venue.cityid === city.id
         )
         setVenues(filteredVenues)
-        // console.log('filteredVenues', filteredVenues)
       } else {
         setVenues([])
       }
@@ -72,10 +70,8 @@ function AddEvent() {
 
   const handleVenueSelect = (venue: DropdownOption | null) => {
     if (venue) {
-      // console.log('Venue selected:', venue)
       setSelectedVenue(venue)
     } else {
-      // console.log('Venue selection cleared')
       setSelectedVenue(null)
     }
   }
@@ -99,8 +95,6 @@ function AddEvent() {
         selectedBands: selectedBands.map((band) => band.id)
       }
 
-      // console.log('formValues', formValues)
-
       const response = await fetch('/addevent', {
         method: 'POST',
         headers: {
@@ -111,6 +105,10 @@ function AddEvent() {
 
       if (response.ok) {
         console.log('Event added successfully')
+        setConfirmationVisible(true)
+        setTimeout(() => {
+          navigate('/')
+        }, 3000)
       } else {
         console.error('Failed to add event')
       }
@@ -122,65 +120,72 @@ function AddEvent() {
   return (
     <>
       <h1 className="add-event-h1">Add event</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          data-testid="event-name"
-          type="text"
-          value={eventName}
-          onChange={(e) => setEventName(e.target.value)}
-          placeholder="Event Name"
-          required
-        />
-        <textarea
-          data-testid="event-description"
-          value={eventDescription}
-          onChange={(e) => setEventDescription(e.target.value)}
-          placeholder="Event Description"
-          required
-        ></textarea>
-        <input
-          data-testid="date"
-          type="date"
-          value={eventDate}
-          onChange={(e) => setEventDate(e.target.value)}
-          required
-        />
-        <input
-          data-testid="time"
-          type="time"
-          value={eventTime}
-          onChange={(e) => setEventTime(e.target.value)}
-          required
-        />
-        <DropdownFiltered
-          testId="city-dropdown"
-          label="Select a city"
-          options={cities}
-          onSelect={handleCitySelect}
-          selectedValue={selectedCity}
-          resetFilterKey={selectedCity?.id}
-        />
-        <DropdownFiltered
-          testId="venue-dropdown"
-          label="Select a venue"
-          options={venues}
-          onSelect={handleVenueSelect}
-          selectedValue={selectedVenue}
-          disabled={!selectedCity}
-          resetFilterKey={selectedVenue?.id}
-        />
-        <BandList bands={bands} onAddBand={handleAddBand} />
-        <SelectedBands selectedBands={selectedBands} />
+      {confirmationVisible ? (
+        <div>
+          <h2>Event added successfully!</h2>
+          <p>You will be redirected to the homepage shortly...</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <input
+            data-testid="event-name"
+            type="text"
+            value={eventName}
+            onChange={(e) => setEventName(e.target.value)}
+            placeholder="Event Name"
+            required
+          />
+          <textarea
+            data-testid="event-description"
+            value={eventDescription}
+            onChange={(e) => setEventDescription(e.target.value)}
+            placeholder="Event Description"
+            required
+          ></textarea>
+          <input
+            data-testid="date"
+            type="date"
+            value={eventDate}
+            onChange={(e) => setEventDate(e.target.value)}
+            required
+          />
+          <input
+            data-testid="time"
+            type="time"
+            value={eventTime}
+            onChange={(e) => setEventTime(e.target.value)}
+            required
+          />
+          <DropdownFiltered
+            testId="city-dropdown"
+            label="Select a city"
+            options={cities}
+            onSelect={handleCitySelect}
+            selectedValue={selectedCity}
+            resetFilterKey={selectedCity?.id}
+          />
+          <DropdownFiltered
+            testId="venue-dropdown"
+            label="Select a venue"
+            options={venues}
+            onSelect={handleVenueSelect}
+            selectedValue={selectedVenue}
+            disabled={!selectedCity}
+            resetFilterKey={selectedVenue?.id}
+          />
+          <BandList bands={bands} onAddBand={handleAddBand} />
+          <SelectedBands selectedBands={selectedBands} />
 
-        <button
-          data-testid="addevent-submit-button"
-          type="submit"
-          disabled={!isFormValid()}
-          className="add-event-submit"
-        >
-          Submit
-        </button>
-      </form>
+          <button
+            data-testid="addevent-submit-button"
+            type="submit"
+            disabled={!isFormValid()}
+            className="add-event-submit"
+          >
+            Submit
+          </button>
+        </form>
+      )}
     </>
   )
 }
