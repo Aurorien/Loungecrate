7a12ac6366d8b01e4e4193e8fb03d5946b25979a
Aurorien <starlightnoise@gmail.com>
2024-01-13 20:27:16 +0100

    Add component test for EventList

--- frontend/cypress/component/EventList.cy.tsx
+++ frontend/cypress/component/EventList.cy.tsx
@@ -17,5 +17,17 @@ describe('Eventlist.tsx', () => {
   it('mounts Eventlist and verifies that it has a title', () => {
     const mockOnEventClick = cy.stub()
     cy.mount(<Eventlist events={events} onEventClick={mockOnEventClick} />)
+    cy.get('[data-testid="event-list-h"]').should('contain', 'My Events')
+    cy.get('[data-testid="event-list"]')
+      .find('[data-testid="event-item"]')
+      .should('have.length', events.length)
+    events.forEach((event, index) => {
+      cy.get(`[data-testid="event-item"]:eq(${index})`)
+        .should('contain', event.eventname)
+        .click()
+        .then(() => {
+          expect(mockOnEventClick).to.have.been.calledWith(event)
+        })
+    })
   })
 })
