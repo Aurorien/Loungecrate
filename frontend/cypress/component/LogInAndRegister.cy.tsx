1812501405d0cf3a78530b5698ea640eadb340f4
Aurorien <starlightnoise@gmail.com>
2024-01-14 21:05:57 +0100

    Add testing error on register

--- frontend/cypress/component/LogInAndRegister.cy.tsx
+++ frontend/cypress/component/LogInAndRegister.cy.tsx
@@ -2,6 +2,13 @@ import LogInAndRegister from '../../src/components/LogInAndRegister'
 
 describe('LogInAndRegister component', () => {
   beforeEach(() => {
+    cy.intercept('POST', '/register', (req) => {
+      if (req.body.username && req.body.password) {
+        req.reply(201, { success: true, message: 'Registration successful' })
+      } else {
+        req.reply(500, 'Internal Server Error')
+      }
+    })
     cy.mount(<LogInAndRegister />)
     cy.get('[data-testid="login"]').should('exist')
     cy.get('[data-testid="register"]').should('not.exist')
@@ -17,6 +24,20 @@ describe('LogInAndRegister component', () => {
 
     cy.get('[data-testid="login"]').should('exist')
     cy.get('[data-testid="register"]').should('not.exist')
+  })
+
+  it('fails to register with invalid username', () => {
+    cy.get('[data-testid="username"]').type('invalid#username')
+    cy.get('[data-testid="password"]').type('validpassword')
+    cy.get('[data-testid="submit"]').click()
+    cy.get('[data-testid="error-message"]').should('exist')
+  })
+
+  it('fails to register with invalid password', () => {
+    cy.get('[data-testid="username"]').type('validusername')
+    cy.get('[data-testid="password"]').type('invalid#password')
+    cy.get('[data-testid="submit"]').click()
+    cy.get('[data-testid="error-message"]').should('exist')
   })
 
   it('submits registration form', () => {
